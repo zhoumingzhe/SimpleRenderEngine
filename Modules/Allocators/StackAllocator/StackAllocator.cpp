8c35f193c453b1ecbad202b18c03936650448e22
Mingzhe Zhou <zhoumingzhe@gmail.com>
2016-08-06 23:35:13 +0800

    StackAllocator, Part 1.

--- Modules/Allocators/StackAllocator/StackAllocator.cpp
+++ Modules/Allocators/StackAllocator/StackAllocator.cpp
@@ -1,36 +1,46 @@
 #include "StackAllocator.h"
 #include <VirtualMemory.h>
 #include <new>
-StackAllocator::StackAllocator(void* base, size_t total)
+#include <assert.h>
+struct StackFrame
+{
+    
+};
+
+template <size_t alignment>
+StackAllocator<alignment>::StackAllocator(size_t total)
 :
-m_base(base),
 m_totalSize(total)
 {
 }
 
-void* StackAllocator::malloc(size_t size)
+template <size_t alignment>
+void* StackAllocator<alignment>::malloc(size_t size)
 {
     return nullptr;
 }
 
-void StackAllocator::free(void* mem)
+template <size_t alignment>
+void StackAllocator<alignment>::free(void* mem)
 {
 
 }
 
-void StackAllocator::destroy()
+template <size_t alignment>
+void StackAllocator<alignment>::destroy()
 {
     FreeVirtualMemory(this, m_totalSize);
 }
 
 //Why allocate the memory outside of the constructor:
 //If no memory, null object will be returned to the outside
-StackAllocator* StackAllocator::CreateStackAllocator(size_t size)
+template <size_t alignment>
+StackAllocator<alignment>* StackAllocator<alignment>::CreateStackAllocator(size_t size)
 {
     void* pMem = AllocateVirtualMemory(size);
     if(pMem)
     {
-        StackAllocator* pAllocator = new (pMem) StackAllocator(pMem, size);
+        StackAllocator* pAllocator = new (pMem) StackAllocator<alignment>(size);
         return pAllocator;
     }
     else
@@ -40,7 +50,27 @@ StackAllocator* StackAllocator::CreateStackAllocator(size_t size)
     }
 }
 
-Allocator* CreateStackAllocator(size_t size)
+Allocator* CreateStackAllocator(size_t size, size_t alignment)
 {
-    return StackAllocator::CreateStackAllocator(size);
+    //align to 4K is enough 
+    assert((0 < alignment) && (alignment <= 12) && (alignment & (alignment - 1)) == 0);
+#define CASE(i) case 1<<(i-1): return StackAllocator< 1<<(i-1) >::CreateStackAllocator(size)
+    switch(alignment)
+    {
+        //nasty code, need to find a better way
+        CASE(1);
+        CASE(2);
+        CASE(3);
+        CASE(4);
+        CASE(5);
+        CASE(6);
+        CASE(7);
+        CASE(8);
+        CASE(9);
+        CASE(10);
+        CASE(12);
+        default:
+            assert(0);
+    }
+    return nullptr;
 }
--- Modules/Allocators/StackAllocator/StackAllocator.h
+++ Modules/Allocators/StackAllocator/StackAllocator.h
@@ -1,16 +1,17 @@
 #include <Allocator.h>
 
+template <size_t alignment>
 class StackAllocator: public Allocator
 {
 public:
-    StackAllocator(void* base, size_t total);
+    StackAllocator(size_t total);
     StackAllocator(const StackAllocator& rhs) = delete;
     void* malloc(size_t size) override;
     void free(void* mem) override;
     void destroy() override;
-
     static StackAllocator* CreateStackAllocator(size_t totalSize);
+
 private:
-    void* m_base;
     size_t m_totalSize;
 };
+
