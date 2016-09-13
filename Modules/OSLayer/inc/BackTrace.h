705c91ff84837ef304bc33ef19b643d5804b5ef7
Mingzhe Zhou <zhoumingzhe@gmail.com>
2016-09-13 16:13:14 +0800

    build on windows

--- Modules/OSLayer/inc/BackTrace.h
+++ Modules/OSLayer/inc/BackTrace.h
@@ -1,4 +1,8 @@
 #include <cstddef>
-
+#ifdef _WIN32
 size_t GetBackTrace(void** buffer, size_t size);
-
+#else
+#include <execinfo.h>
+//Avoid 1 more back trace generated in function call
+#define GetBackTrace(buffer, size) backtrace(buffer, size)
+#endif
